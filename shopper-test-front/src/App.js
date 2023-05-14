056247ba5f77086eefa805d1045ea43ac1d9c1af
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-05-14 16:21:19 -0300

    install and configure some frameworks and creat some folders

--- shopper-test-front/src/App.js
+++ shopper-test-front/src/App.js
@@ -1,8 +1,11 @@
-
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <h1>Ta funcionando</h1>
+    <Routes>
+          <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ shopper-test-front/src/pages/Home/index.js
@@ -0,0 +1,7 @@
+export function Home() {
+
+  return (
+    <h1>Tamo na home</h1>
+  )
+
+}
