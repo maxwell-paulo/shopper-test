9f5d1ff3bd3dd9e2a7580cc76c91942c7c02277f
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-05-15 14:20:45 -0300

    show to user if something is wrong in his form

--- shopper-test-front/src/App.js
+++ shopper-test-front/src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
+  <div>
+    <Toaster position="top-center" reverseOrder={false} />
     <Routes>
           <Route path="/" element={<Home />} />
     </Routes>
+    </div>
   );
 }
 
--- shopper-test-front/src/pages/Home/index.js
+++ shopper-test-front/src/pages/Home/index.js
@@ -1,51 +1,66 @@
 import { useState, useEffect } from "react";
 import { api } from "../../api/api.js";
-
+import toast from "react-hot-toast";
 
 export function Home() {
   const [form, setForm] = useState({
     code: "",
     sales_price: ""
-  })
+  });
+
+  const [camposPreenchidos, setCamposPreenchidos] = useState(false);
+
+  useEffect(() => {
+    setCamposPreenchidos(form.code.length > 0 && form.sales_price.length > 0);
+  }, [form.code, form.sales_price]);
 
   function handleChange(e) {
-    setForm({...form, [e.target.name]: e.target.value})
-
+    setForm({ ...form, [e.target.name]: e.target.value });
   }
 
-
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const floatValue = parseFloat(form.sales_price);
     if (isNaN(floatValue)) {
-      console.error('O valor inserido não é um número de ponto flutuante válido');
+      toast.error('O valor inserido não é um número válido');
       return;
     }
 
     try {
       await api.put(`${form.code}`, form);
-
     } catch (err) {
-      console.log(err);
+      toast.error(err.response.data);
     }
-
   }
-
-
 
   return (
     <div>
       <h1>Atualizar o preço do produto:</h1>
       <form onSubmit={handleSubmit}>
-      <label htmlFor="codigo">Código do produto:</label>
-      <input id="codigo" name="code" step="1" type="number" value={form.code} onChange={handleChange} />
-      <label htmlFor="preco">Novo preço do produto:</label>
-      <input id="preco" name="sales_price" type="text" value={form.sales_price} onChange={handleChange}/>
-      <button type="submit">Atualizar produto</button>
+        <label htmlFor="codigo">Código do produto:</label>
+        <input
+          required
+          id="codigo"
+          name="code"
+          step="1"
+          type="number"
+          value={form.code}
+          onChange={handleChange}
+        />
+        <label htmlFor="preco">Novo preço do produto:</label>
+        <input
+          required
+          id="preco"
+          name="sales_price"
+          type="text"
+          value={form.sales_price}
+          onChange={handleChange}
+        />
+        <button type="submit" disabled={!camposPreenchidos}>
+          Atualizar produto
+        </button>
       </form>
-
     </div>
-  )
-
+  );
 }
